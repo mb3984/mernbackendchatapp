983007f810134d433e0a91756a1b7045ab5b71ef
madhu <madhubabu@gmail.com>
2025-05-07 09:33:38 +0530

    addded/route

--- server.js
+++ server.js
@@ -26,3 +26,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("welcome to backend mern chat app");
+});
